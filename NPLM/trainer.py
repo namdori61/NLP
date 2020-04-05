c2e93779f7a0b750c7f093f37278f110fd8cd668
Sangho.Nam <namdori61@gmail.com>
2020-04-05 13:17:33 +0900

    Feat: check_points on trainer

--- NPLM/trainer.py
+++ NPLM/trainer.py
@@ -1,23 +1,31 @@
-def trainer(model, loss_func, optimizer, dataloader, device, num_epochs):
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from utils import *
 
-    losses = []
-    for epo in range(num_epochs):
-        total_loss = .0
-        for i, v in enumerate(dataloader):
-            contexts = v[0].to(device)
-            target = v[1].to(device)
-            
-            model.zero_grad()
+def trainer(model, loss_func, optimizer, dataloader, device, num_epochs, check_points=None):
+    if check_points is not None:
+        load_check_points(check_points, model, optimizer, device, mode='train')
+    else:
+        losses = []
+        for epo in range(num_epochs):
+            total_loss = .0
+            for i, v in enumerate(dataloader):
+                contexts = v[0].to(device)
+                target = v[1].to(device)
+                
+                model.zero_grad()
 
-            probs = model(contexts)
+                probs = model(contexts)
 
-            loss = loss_func(probs, target)
+                loss = loss_func(probs, target)
 
-            loss.backward()
-            optimizer.step()
+                loss.backward()
+                optimizer.step()
 
-            total_loss += loss.item()
-        losses.append(total_loss)
-        print(epo, 'th epoch loss :', total_loss)
+                total_loss += loss.item()
+            losses.append(total_loss)
+            print(epo, 'th epoch loss :', total_loss)
+        
+        save_check_points(check_points, model, num_epochs, total_loss)
     
     return model, losses
